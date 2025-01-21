b8817f671de8cf1f9f788baf7ae3a0b4683ae7ad
okoliken <jahbuchimokoli3@gmail.com>
2025-01-21 22:46:24 +0100

    removed unused imports

--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,12 +1,12 @@
 "use client";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Card } from "@/components/ui/card";
 
 
 const SignUpPage = () => {
     return (
         <Card className="w-full max-w-[623px]">
 
-            
+
         </Card>
     )
 } 
