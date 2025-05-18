d0a5d955c14eed4d655492f3ca82346678f93fcb
okoliken <jahbuchimokoli3@gmail.com>
2025-05-18 23:31:44 +0100

    implementing auth fow

--- components/notes/ui/NoteList.tsx
+++ components/notes/ui/NoteList.tsx
@@ -9,7 +9,7 @@ import { Note } from "@/types/notes";
 const NoteList = () => {
   const { isArchived } = useNotes();
   const notes = useNoteStore((state) => state.notes);
-  const toggleSidePanel = useNoteStore((state) => state.toggleSidePanel); 
+  const toggleSidePanel = useNoteStore((state) => state.toggleSidePanel);
   const selectNote = useNoteStore((state) => state.selectNote);
   const addNote = useNoteStore((state) => state.addNote);
 
@@ -21,15 +21,18 @@ const NoteList = () => {
       id: Math.random().toString(36).substring(2, 15),
       tags: [],
     };
-    toggleSidePanel()
+    toggleSidePanel();
     addNote(newNote);
     selectNote(newNote.id);
-  }; 
+  };
 
   return (
     <>
       <div className="flex flex-col gap-4 py-5 px-4 xl:pl-[35px] xl:pr-4 bg-white dark:bg-[#0E121B]">
-        <Button onClick={createUntitledNote} className={"hidden md:block w-full"}>
+        <Button
+          onClick={createUntitledNote}
+          className={"hidden md:block w-full"}
+        >
           + Create New Note
         </Button>
 
@@ -41,16 +44,17 @@ const NoteList = () => {
           ) : (
             <div className="flex items-center justify-center h-full bg-base-100 dark:bg-base-800 text-base-950 p-2 border border-base-200 dark:border-base-700 rounded-lg dark:text-white">
               {!isArchived ? (
-                 <p className="text-base-950 dark:text-white text-sm tracking-[-0.2px]">
-                 You don't have any notes yet. Start a new note to capture your
-                 thoughts and ideas.
-               </p>
+                <p className="text-base-950 dark:text-white text-sm tracking-[-0.2px]">
+                  You don't have any notes yet. Start a new note to capture your
+                  thoughts and ideas.
+                </p>
               ) : (
                 <p className="text-base-950 dark:text-white text-sm tracking-[-0.2px]">
-              No notes have been archived yet. Move notes here for safekeeping, <span className="underline">or create a new note</span>.
-              </p>
+                  No notes have been archived yet. Move notes here for
+                  safekeeping,{" "}
+                  <span className="underline">or create a new note</span>.
+                </p>
               )}
-             
             </div>
           )}
         </ScrollArea>
@@ -62,9 +66,10 @@ const NoteList = () => {
 const ArichivedBanner = () => {
   return (
     <p className="text-sm tracking-tight dark:text-base-200">
-      All your archived notes are stored here. You can restore or delete them anytime.
+      All your archived notes are stored here. You can restore or delete them
+      anytime.
     </p>
-  )
-}
+  );
+};
 
 export default NoteList;
